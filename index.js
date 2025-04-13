1c895b91b042e4efc633a4351f7bdd028e0a725b
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2025-04-13 20:33:03 +0100

    Setup: Github pages site to go live in a basic form.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.info(
+	`
+		This site is within the earliest stages of development.
+		Nonetheless, I am hoping to add various embellishments as soon
+		as possible! Watch this space...
+	`
+);
